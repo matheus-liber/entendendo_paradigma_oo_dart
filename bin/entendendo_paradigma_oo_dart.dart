7bc4c246bd0f9b2c87333d7a28f1ea19704d3004
matheus-liber <matheuslibergt@gmail.com>
2023-05-19 16:20:52 -0300

    Inicialização do projeto

---
+++ bin/entendendo_paradigma_oo_dart.dart
@@ -0,0 +1,5 @@
+import 'package:entendendo_paradigma_oo_dart/entendendo_paradigma_oo_dart.dart' as entendendo_paradigma_oo_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${entendendo_paradigma_oo_dart.calculate()}!');
+}
---
+++ test/entendendo_paradigma_oo_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:entendendo_paradigma_oo_dart/entendendo_paradigma_oo_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
