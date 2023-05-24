41d25b0d3300395c369328914c48d323b0011a52
matheus-liber <matheuslibergt@gmail.com>
2023-05-24 16:35:46 -0300

    Entendendo classes

--- bin/entendendo_paradigma_oo_dart.dart
+++ bin/entendendo_paradigma_oo_dart.dart
@@ -1,12 +1,36 @@
 void main() {
-  String nome = "Laranja";
-  double peso = 100.2;
-  String cor = "Verde e Amarela";
-  String sabor = "Adocicada e cítrica";
-  int diasDesdeColheita = 23;
-  bool isMadura = funcEstaMadura(diasDesdeColheita);
+  Fruta fruta1 = Fruta("Laranja", 100.2, "Verde e Amarela", "Adocicada e cítrica", 23);
+  print(fruta1.isMadura);
 
-  print(isMadura);
+} 
+
+class Fruta{
+  String nome;
+  double peso;
+  String cor;
+  String sabor;
+  int diasDesdeColheita;
+  bool? isMadura;
+
+  Fruta(this.nome, this.peso, this.cor, this.sabor, this.diasDesdeColheita, {this.isMadura});
+
+  estaMadura(int diasParaMadura){
+    isMadura = diasDesdeColheita >= diasParaMadura;
+    print('A sua $nome foi colhida a $diasDesdeColheita dias, e precisa de $diasParaMadura dias para amadurecer. Portanto é $isMadura que ela é comestível.');
+  }
+}
+
+functQuantosDiasMadura(int dias){
+  int diasParaMadura = 30;
+  int quantosDiasFaltam = dias - diasParaMadura;
+}
+
+void mostrarMadura(String nome, int dias) {
+  if (dias >= 30) {
+    print('A $nome está madura.');
+  } else {
+    print('A $nome não está madura.');
+  }
 }
 
 bool funcEstaMadura(int dias){
