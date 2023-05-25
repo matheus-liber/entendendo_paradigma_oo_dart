75fc2f184a00ac19ac06bcaeed174e12493f8316
matheus-liber <matheuslibergt@gmail.com>
2023-05-25 16:22:14 -0300

    Curso finalizado

--- bin/entendendo_paradigma_oo_dart.dart
+++ bin/entendendo_paradigma_oo_dart.dart
@@ -2,6 +2,20 @@ void main() {
   Fruta fruta1 = Fruta("Laranja", 100.2, "Verde e Amarela", "Adocicada e cítrica", 23);
   print(fruta1.isMadura);
 
+  Legume mandioca = Legume('Macaxeira', 1200, 'Marrom', true);
+  Fruta banana = Fruta('Banana', 75, 'Amarela', 'Doce', 12);
+  Noz macadamia = Noz('Macadamia', 2, 'Branco Amarelado', 'Doce', 20, 35);
+  Citrica limao = Citrica('Limão', 50, 'Verde', 'Azedo', 5, 9);
+
+  mandioca.printAlimento();
+  banana.printAlimento();
+  macadamia.printAlimento();
+  limao.printAlimento();
+
+  mandioca.cozinhar();
+  banana.fazerSuco();
+  limao.existeRefri(true);
+
 } 
 
 class Alimento{
@@ -11,11 +25,11 @@ class Alimento{
   Alimento(this.nome,this.peso,this.cor);
 
   void printAlimento(){
-    print('Este(a) $nome pesa $peso gramas e é $cor.')
+    print('Este(a) $nome pesa $peso gramas e é $cor.');
   }
 }
 
-class Legume extends Alimento {
+class Legume extends Alimento implements Bolo{
   bool isPrecisaCozinhar;
 
   Legume(String nome, double peso, String cor, this.isPrecisaCozinhar)
@@ -28,9 +42,21 @@ class Legume extends Alimento {
       print('Não precisa cozinhar');
     }
   }
+  @override
+  void separarIngredientes() {
+
+  }
+  @override
+  void fazerMassa() {
+
+  }
+  @override
+  void assar() {
+
+  }
 }
 
-class Fruta extends Alimento{
+class Fruta extends Alimento implements Bolo{
   String sabor;
   int diasDesdeColheita;
   bool? isMadura;
@@ -46,25 +72,49 @@ class Fruta extends Alimento{
   void fazerSuco(){
     print('Você fez um ótimo suco de $nome');
   }
-}
-
-functQuantosDiasMadura(int dias){
-  int diasParaMadura = 30;
-  int quantosDiasFaltam = dias - diasParaMadura;
-}
-
-void mostrarMadura(String nome, int dias) {
-  if (dias >= 30) {
-    print('A $nome está madura.');
-  } else {
-    print('A $nome não está madura.');
+  @override
+  void separarIngredientes(){
+    print('Pegar a fruta');
+  }
+  @override
+  void fazerMassa(){
+    print('Misturar a fruta com farinha, açucar, ovos, etc');
+  }
+  @override
+  void assar(){
+    print('Colocar para assar no forno por 40 minutos');
   }
 }
 
-bool funcEstaMadura(int dias){
-  if (dias >= 20){
-      return true;
+class Citrica extends Fruta{
+  double nivelAzedo;
+
+  Citrica(String nome, double peso, String cor, String sabor, int diasDesdeColheita, this.nivelAzedo) 
+    : super(nome, peso, cor, sabor, diasDesdeColheita);
+
+  void existeRefri(bool existe){
+    if(existe){
+      print('Existe refrigerante de $nome');
     } else {
-      return false;
+      print('Não existe refrigerante de $nome');
+    }
   }
 }
+
+class Noz extends Fruta{
+  double porcentagemOleoNatural;
+
+  Noz(String nome, double peso, String cor, String sabor, int diasDesdeColheita, this.porcentagemOleoNatural) 
+    : super(nome, peso, cor, sabor, diasDesdeColheita);
+}
+
+abstract class Bolo {
+  void separarIngredientes();
+
+  void fazerMassa();
+
+  void assar();
+}
+
+
+
