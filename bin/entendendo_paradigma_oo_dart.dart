638008bc91f2959e7b583abc410b2d37bcbf3c0e
matheus-liber <matheuslibergt@gmail.com>
2023-05-25 15:29:00 -0300

    Estudando Herança no Dart

--- bin/entendendo_paradigma_oo_dart.dart
+++ bin/entendendo_paradigma_oo_dart.dart
@@ -4,19 +4,47 @@ void main() {
 
 } 
 
-class Fruta{
+class Alimento{
   String nome;
   double peso;
   String cor;
+  Alimento(this.nome,this.peso,this.cor);
+
+  void printAlimento(){
+    print('Este(a) $nome pesa $peso gramas e é $cor.')
+  }
+}
+
+class Legume extends Alimento {
+  bool isPrecisaCozinhar;
+
+  Legume(String nome, double peso, String cor, this.isPrecisaCozinhar)
+    : super(nome, peso, cor);
+
+  void cozinhar(){
+    if(isPrecisaCozinhar){
+      print('Pronto, o $nome está cozinhando');
+    } else {
+      print('Não precisa cozinhar');
+    }
+  }
+}
+
+class Fruta extends Alimento{
   String sabor;
   int diasDesdeColheita;
   bool? isMadura;
 
-  Fruta(this.nome, this.peso, this.cor, this.sabor, this.diasDesdeColheita, {this.isMadura});
+  Fruta(String nome, double peso, String cor, this.sabor, this.diasDesdeColheita, {this.isMadura})
+    : super(nome, peso, cor);
 
   estaMadura(int diasParaMadura){
     isMadura = diasDesdeColheita >= diasParaMadura;
     print('A sua $nome foi colhida a $diasDesdeColheita dias, e precisa de $diasParaMadura dias para amadurecer. Portanto é $isMadura que ela é comestível.');
+  }
+
+  void fazerSuco(){
+    print('Você fez um ótimo suco de $nome');
   }
 }
 
